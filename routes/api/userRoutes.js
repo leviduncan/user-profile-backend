0f4e91feffd307389b47ce9d40f296109a7aa3ac
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-01-18 22:12:35 -0500

    Add some comments

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/usermodel')
 
+// Create a new user
 router.post('/add', (req, res) => {
     const newUser = new User({
         title: req.body.title,
@@ -17,24 +18,28 @@ router.post('/add', (req, res) => {
         .catch(err => res.status(400).json(err))
 })
 
+// Get all users
 router.get('/', (req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json(err))
 })
 
+// Get an individual user based on id
 router.get('/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => res.json(user))
         .catch(err => res.status(400).json(err))
 })
 
+// Update an individual user based on id
 router.put('/:id', (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(user => res.json(user))
         .catch(err => res.status(400).json(err))
 })
 
+// Delete an individual user based on id
 router.delete('/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id)
         .then(() => res.json({ success: true }))
